fcddae2c48ba176cef5bd6620fa850746cf9a24e
Daniel Joseph <danielrjoseph8@gmail.com>
2025-03-24 12:53:14 -0400

    Corrected build structure

---
+++ phase2-w25/src/semantic/semantic.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../../include/parser.h"
+#include "../../include/lexer.h"
+#include "../../include/tokens.h"
+#include "../../include/semantic.h"
+
